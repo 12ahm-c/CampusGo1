0ca3a38b609e7d6b7324b28194c5149802de2fad
Ahmed Ismail <ahismail2005@gmail.com>
2026-03-10 18:16:49 +0000

    lf

--- src/services/liveMapService.js
+++ src/services/liveMapService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; // عدّل حسب سيرفرك
+// هذا يأخذ الرابط مباشرة من environment variables
+const API_URL = import.meta.env.VITE_API_URL_AUTH.replace("/auth", "");
 
 export const getAllBuses = async () => {
   try {
--- src/services/scheduleService.js
+++ src/services/scheduleService.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL_SCHEDULE;
+
 export const getSchedules = async () => {
-  const response = await axios.get('http://localhost:3000/api/schedule');
-  return response.data;
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 };
